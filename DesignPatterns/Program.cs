9edbebf305163deea58f2a30d257784125db5168
brandon.davis <brandon.davis@darkmatter2bd.com>
2022-05-26 12:41:06 -0400

    Add project files.

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DesignPatterns.Intro_To_Design_Patterns;
+
+MallardDuck Duck = new MallardDuck();
+Duck.Display();
+Duck.Quack();
+Duck.Swim();
+
+Console.WriteLine("----------------------------------------------");
+
+RedheadDuck Duck2 = new RedheadDuck();
+Duck2.Display();
+Duck2.Quack();
+Duck2.Swim();
+
+
+Console.ReadKey();
