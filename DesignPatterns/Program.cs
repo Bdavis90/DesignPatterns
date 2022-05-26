7af727353906dfb0303ee198bb865b917ffdbbe6
brandon.davis <brandon.davis@darkmatter2bd.com>
2022-05-26 14:26:33 -0400

    Created classes to fix

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,17 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
 using DesignPatterns.Intro_To_Design_Patterns;
 
-MallardDuck Duck = new MallardDuck();
-Duck.Display();
-Duck.Quack();
-Duck.Swim();
+MallardDuck MDuck = new MallardDuck();
+MDuck.Display();
+MDuck.Quack();
+MDuck.Swim();
+MDuck.Fly();
+
 
 Console.WriteLine("----------------------------------------------");
 
-RedheadDuck Duck2 = new RedheadDuck();
-Duck2.Display();
-Duck2.Quack();
-Duck2.Swim();
+RedheadDuck RDuck = new RedheadDuck();
+RDuck.Display();
+RDuck.Quack();
+RDuck.Swim();
+RDuck.Fly();
 
+Console.WriteLine("----------------------------------------------");
+
+RubberDuck RubDuck = new RubberDuck();
+RubDuck.Display();
+RubDuck.Quack();
+RubDuck.Swim();
+RubDuck.Fly();
+
+Console.WriteLine("----------------------------------------------");
+
+DecoyDuck WDuck = new DecoyDuck();
+WDuck.Display();
+WDuck.Quack();
+WDuck.Swim();
+WDuck.Fly();
 
 Console.ReadKey();
---
+++ IntroToDesignPatterns/Intro To Design Patterns/Duck.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Intro_To_Design_Patterns
+{
+    public class Duck
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine("I am a Duck");
+        }
+
+        public virtual void Quack()
+        {
+            Console.WriteLine("Quacking!");
+        }
+
+        public virtual void Swim()
+        {
+            Console.WriteLine("Swimming!");
+        }
+
+        public virtual void Fly()
+        {
+            Console.WriteLine("Flying!");
+        }
+    }
+
+
+
+    public class MallardDuck : Duck
+    {
+        public override void Display()
+        {
+            Console.WriteLine("I am a Mallard Duck!");
+        }
+    }
+
+    public class RedheadDuck : Duck
+    {
+        public override void Display()
+        {
+            Console.WriteLine("I am a Redhead Duck!");
+        }
+    }
+
+    public class RubberDuck : Duck
+    {
+        public override void Display()
+        {
+            Console.WriteLine("I am a Rubber Duck!");
+        }
+
+        public override void Quack()
+        {
+            Console.WriteLine("I Squeak!");
+        }
+
+        public override void Fly()
+        {
+            Console.WriteLine("I don't fly. :(");
+        }
+    }
+
+    public class DecoyDuck : Duck
+    {
+        public override void Quack()
+        {
+            Console.WriteLine("I don't quack.");
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("I am a Decoy Duck!");
+        }
+
+        public override void Fly()
+        {
+            Console.WriteLine("I don't fly. :(");
+        }
+    }
+}
